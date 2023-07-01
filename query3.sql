d6f701f49ca9506bfd1bee7fdf5fe870d7c537bd
Roman Croitor <Roman.Croitor@gmail.com>
2023-07-01 23:49:10 +0200

    Update query3.sql

--- query3.sql
+++ query3.sql
@@ -1,4 +1,4 @@
-﻿SELECT CodDept, AVG(ValFon/SrfIm)::decimal(6,2) AS prix_m2
+SELECT CodDept, Round (Avg(ValFon / SrfIm), 2) AS prix_m2
 FROM immobilier
 JOIN department ON department.id = immobilier.id
 GROUP BY department.CodDept
