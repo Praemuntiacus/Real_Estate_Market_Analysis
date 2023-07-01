94a90e5948237e7cb0a035cf71878c0087471da9
Roman Croitor <Roman.Croitor@gmail.com>
2023-07-01 23:51:08 +0200

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,6 +1,5 @@
-﻿SELECT CodDept, AVG(ValFon/SrfIm)::decimal(6,2) AS prix_m2
-FROM immobilier
-JOIN department ON department.id = immobilier.id
-WHERE TypIm = 'Maison'
-GROUP BY department.CodDept
-HAVING CodDept IN ('75', '77', '78', '91', '92', '93', '94', '95');
+SELECT Round(( tab1.prix_m2_total / tab2.srf_total ), 2) AS prix_m2_maison_IDF
+FROM (SELECT TypIm, Sum(ValFon) AS prix_m2_total FROM immobilier LEFT JOIN department ON immobilier.id_dept = department.id WHERE TypIm = 'Maison' AND CodDept IN ( '75', '77', '78', '91', '92', '93', '94', '95' )
+GROUP BY immobilier.typim) AS tab1,
+(SELECT TypIm, Sum(SrfIm) AS srf_total FROM immobilier LEFT JOIN department ON immobilier.id_dept = department.id WHERE TypIm = 'Maison' AND CodDept IN ( '75', '77', '78', '91', '92', '93', '94', '95' )
+GROUP BY immobilier.typim) AS tab2;
