b5c22f860b300e7f0c4bfa74cb4d81a0d58346b9
Roman Croitor <Roman.Croitor@gmail.com>
2023-07-01 23:56:00 +0200

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,5 +1,15 @@
-SELECT Round(( tab1.prix_m2_total / tab2.srf_total ), 2) AS prix_m2_maison_IDF
-FROM (SELECT TypIm, Sum(ValFon) AS prix_m2_total FROM immobilier LEFT JOIN department ON immobilier.id_dept = department.id WHERE TypIm = 'Maison' AND CodDept IN ( '75', '77', '78', '91', '92', '93', '94', '95' )
-GROUP BY immobilier.typim) AS tab1,
-(SELECT TypIm, Sum(SrfIm) AS srf_total FROM immobilier LEFT JOIN department ON immobilier.id_dept = department.id WHERE TypIm = 'Maison' AND CodDept IN ( '75', '77', '78', '91', '92', '93', '94', '95' )
-GROUP BY immobilier.typim) AS tab2;
+SELECT ROUND((tab1.prix_m2_total / tab2.srf_total), 2) AS prix_m2_maison_IDF
+FROM
+  (SELECT TypIm, SUM(ValFon) AS prix_m2_total
+   FROM immobilier
+   LEFT JOIN department ON immobilier.id_dept = department.id
+   WHERE TypIm = 'Maison' AND CodDept IN ('75', '77', '78', '91', '92', '93', '94', '95')
+   GROUP BY immobilier.TypIm) AS tab1
+JOIN
+  (SELECT TypIm, SUM(SrfIm) AS srf_total
+   FROM immobilier
+   LEFT JOIN department ON immobilier.id_dept = department.id
+   WHERE TypIm = 'Maison' AND CodDept IN ('75', '77', '78', '91', '92', '93', '94', '95')
+   GROUP BY immobilier.TypIm) AS tab2
+ON tab1.TypIm = tab2.TypIm;
+
