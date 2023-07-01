50749919e26637559f5bf58d9dd7d0c6f75e3a2f
Roman Croitor <Roman.Croitor@gmail.com>
2023-07-01 23:46:43 +0200

    Update query1.sql

--- query1.sql
+++ query1.sql
@@ -1,5 +1,4 @@
-﻿SELECT COUNT(DatMut) AS apparts_vendus
-FROM mutation
-JOIN immobilier ON immobilier.id = mutation.id
-WHERE TypIm = 'Appartement' AND NatMut = 'Vente' AND DatMut BETWEEN '01-01-2020' AND '30-06-2020'
-;
+SELECT Count(immobilier.id) AS apparts_vendus 
+FROM immobilier
+LEFT JOIN mutation ON mutation.id = immobilier.id
+WHERE TypIm = 'Appartement' AND NatMut = 'Vente' AND DatMut BETWEEN '01-01-2020' AND '30-06-2020';
