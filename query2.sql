7632cd725a61ae15015c21da545c7a83600ecec0
Roman Croitor <Roman.Croitor@gmail.com>
2023-07-01 23:47:53 +0200

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,4 +1,4 @@
-﻿SELECT NbPiece AS pieces, COUNT(NbPiece) as apparts
+SELECT NbPiece AS pieces, Count(NbPiece) AS nombr_appart, ( Count(NbPiece) * 100 / (SELECT Count(id) FROM immobilier WHERE TypIm = 'Appartement') ) AS prsnt_appart
 FROM immobilier
 WHERE TypIm = 'Appartement'
 GROUP BY NbPiece
