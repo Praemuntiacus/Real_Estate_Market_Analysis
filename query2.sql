cba30536e1b3a3997c9e4e6607c04ae96d48ae86
Roman Croitor <Roman.Croitor@gmail.com>
2023-07-02 02:30:55 +0200

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,4 +1,4 @@
-SELECT NbPiece AS pieces, Count(NbPiece) AS nombr_appart, ( Count(NbPiece) * 100 / (SELECT Count(id) FROM immobilier WHERE TypIm = 'Appartement') ) AS prsnt_appart
+SELECT NbPiece AS pieces, COUNT(NbPiece) AS nombr_appart, (COUNT(NbPiece) * 100 / (SELECT COUNT(id) FROM immobilier WHERE TypIm = 'Appartement')) AS prsnt_appart
 FROM immobilier
 WHERE TypIm = 'Appartement'
 GROUP BY NbPiece
